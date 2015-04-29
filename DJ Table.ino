996bcc66e628374a8728f573353c47ed305bf123
Presence <presence@OfficeBSD.entertainment.local>
2015-04-28 20:03:18 -0700

    Updated to exact number of LEDs in table

--- DJ Table.ino
+++ DJ Table.ino
@@ -23,10 +23,10 @@ float potSmoothing = 0.1; // smaller value filters variances more
 Adafruit_NeoPixel controller = Adafruit_NeoPixel(3, 5, NEO_RGB + NEO_KHZ400);
 
 /* Neopixel WS2812B Top Strip on pin 7 (white**********************************/
-Adafruit_NeoPixel stripT = Adafruit_NeoPixel(150, 7, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripT = Adafruit_NeoPixel(104, 7, NEO_GRB + NEO_KHZ800);
 
 /* Neopixel WS2812B Bottom Strip on pin 6 (yellow) ****************************/
-Adafruit_NeoPixel stripB = Adafruit_NeoPixel(150, 6, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripB = Adafruit_NeoPixel(84, 6, NEO_GRB + NEO_KHZ800);
 
 
 /* Buttons wired to ground from pins 11 and 12 ********************************/
@@ -465,55 +465,55 @@ void PlasmaStrip () {
 }
 
 void PlasmaController () {
-	  phase += phaseIncrement;
-  // The two points move along Lissajious curves, see: http://en.wikipedia.org/wiki/Lissajous_curve
-  // We want values that fit the LED grid: x values between 0..13, y values between 0..8 .
-  // The sin() function returns values in the range of -1.0..1.0, so scale these to our desired ranges.
-  // The phase value is multiplied by various constants; I chose these semi-randomly, to produce a nice motion.
-  Point p1 = { (sin(phase*1.000)+1.0) * 4.5, (sin(phase*1.310)+1.0) * 4.0 };
-  Point p2 = { (sin(phase*1.770)+1.0) * 4.5, (sin(phase*2.865)+1.0) * 4.0 };
-  Point p3 = { (sin(phase*0.250)+1.0) * 4.5, (sin(phase*0.750)+1.0) * 4.0 };
-  
-  byte row, col;
-  
-  // For each row...
-  for( row=0; row<1; row++ ) {
-    float row_f = float(row);  // Optimization: Keep a floating point value of the row number, instead of recasting it repeatedly.
-    
-    // For each column...
-    for( col=0; col<controller.numPixels(); col++ ) {
-      float col_f = float(col);  // Optimization.
-      
-      // Calculate the distance between this LED, and p1.
-      Point dist1 = { col_f - p1.x, row_f - p1.y };  // The vector from p1 to this LED.
-      float distance1 = sqrt( dist1.x*dist1.x + dist1.y*dist1.y );
-      
-      // Calculate the distance between this LED, and p2.
-      Point dist2 = { col_f - p2.x, row_f - p2.y };  // The vector from p2 to this LED.
-      float distance2 = sqrt( dist2.x*dist2.x + dist2.y*dist2.y );
-      
-      // Calculate the distance between this LED, and p3.
-      Point dist3 = { col_f - p3.x, row_f - p3.y };  // The vector from p3 to this LED.
-      float distance3 = sqrt( dist3.x*dist3.x + dist3.y*dist3.y );
-      
-      // Warp the distance with a sin() function. As the distance value increases, the LEDs will get light,dark,light,dark,etc...
-      // You can use a cos() for slightly different shading, or experiment with other functions. Go crazy!
-      float color_1 = distance1;  // range: 0.0...1.0
-      float color_2 = distance2;
-      float color_3 = distance3;
-      float color_4 = (sin( distance1 * distance2 * colorStretch )) + 2.0 * 0.5;
-      
-      // Square the color_f value to weight it towards 0. The image will be darker and have higher contrast.
-      color_1 *= color_1 * color_4;
-      color_2 *= color_2 * color_4;
-      color_3 *= color_3 * color_4;
-      color_4 *= color_4;
- 
-      // Scale the color up to 0..7 . Max brightness is 7.
-      //controller.setPixelColor(col + (8 * row), controller.Color(color_4, 0, 0) );
-      controller.setPixelColor(col + (3 * row), controller.Color(color_1, color_2, color_3));
-    }
-  }
-  controller.show();
+	phase += phaseIncrement;
+	// The two points move along Lissajious curves, see: http://en.wikipedia.org/wiki/Lissajous_curve
+	// We want values that fit the LED grid: x values between 0..13, y values between 0..8 .
+	// The sin() function returns values in the range of -1.0..1.0, so scale these to our desired ranges.
+	// The phase value is multiplied by various constants; I chose these semi-randomly, to produce a nice motion.
+	Point p1 = { (sin(phase*1.000)+1.0) * 4.5, (sin(phase*1.310)+1.0) * 4.0 };
+	Point p2 = { (sin(phase*1.770)+1.0) * 4.5, (sin(phase*2.865)+1.0) * 4.0 };
+	Point p3 = { (sin(phase*0.250)+1.0) * 4.5, (sin(phase*0.750)+1.0) * 4.0 };
+
+	byte row, col;
+
+	// For each row...
+	for( row=0; row<1; row++ ) {
+		float row_f = float(row);  // Optimization: Keep a floating point value of the row number, instead of recasting it repeatedly.
+
+		// For each column...
+		for( col=0; col<controller.numPixels(); col++ ) {
+			float col_f = float(col);  // Optimization.
+
+			// Calculate the distance between this LED, and p1.
+			Point dist1 = { col_f - p1.x, row_f - p1.y };  // The vector from p1 to this LED.
+			float distance1 = sqrt( dist1.x*dist1.x + dist1.y*dist1.y );
+
+			// Calculate the distance between this LED, and p2.
+			Point dist2 = { col_f - p2.x, row_f - p2.y };  // The vector from p2 to this LED.
+			float distance2 = sqrt( dist2.x*dist2.x + dist2.y*dist2.y );
+
+			// Calculate the distance between this LED, and p3.
+			Point dist3 = { col_f - p3.x, row_f - p3.y };  // The vector from p3 to this LED.
+			float distance3 = sqrt( dist3.x*dist3.x + dist3.y*dist3.y );
+
+			// Warp the distance with a sin() function. As the distance value increases, the LEDs will get light,dark,light,dark,etc...
+			// You can use a cos() for slightly different shading, or experiment with other functions. Go crazy!
+			float color_1 = distance1;  // range: 0.0...1.0
+			float color_2 = distance2;
+			float color_3 = distance3;
+			float color_4 = (sin( distance1 * distance2 * colorStretch )) + 2.0 * 0.5;
+
+			// Square the color_f value to weight it towards 0. The image will be darker and have higher contrast.
+			color_1 *= color_1 * color_4;
+			color_2 *= color_2 * color_4;
+			color_3 *= color_3 * color_4;
+			color_4 *= color_4;
+
+			// Scale the color up to 0..7 . Max brightness is 7.
+			//controller.setPixelColor(col + (8 * row), controller.Color(color_4, 0, 0) );
+			controller.setPixelColor(col + (3 * row), controller.Color(color_1, color_2, color_3));
+		}
+	}
+	controller.show();
 }
 
